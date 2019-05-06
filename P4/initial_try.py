356b5b7065ffcc8f52ff1cf24e95b2aa4d93a6c4
Benton Liang <benton_liang@college.harvard.edu>
2019-05-06 04:57:50 -0400

    im tired

--- P4/initial_try.py
+++ P4/initial_try.py
@@ -17,15 +17,20 @@ class Learner(object):
         self.last_reward = None
 
         self.eta = 0.05
-        self.gamma = 0.70
+        self.gamma = 0.7
         self.epsilon = 0.01
+        self.gravity = 0
         self.qtable = {}
+
 
     def reset(self):
         self.last_state  = None
         self.last_action = None
         self.last_reward = None
-        self.epsilon *= 0.99
+        self.epsilon *= 0.995
+        # print(self.gravity)
+        self.gravity = None
+        # print(self.qtable)
 
     def action_callback(self, state):
         '''
@@ -38,9 +43,19 @@ class Learner(object):
         # You'll need to select and action and return it.
         # Return 0 to swing and 1 to jump.
 
-        new_state = (state["monkey"]["vel"] // 20,
-                     state["tree"]["dist"] // 200,
-                     np.mean([state["tree"]["top"] - state["monkey"]["top"], state["tree"]["bot"] - state["monkey"]["bot"]]) // 100)
+        # don't jump at the first frame, determine gravity
+        if not self.gravity:
+            self.gravity = -state["monkey"]["vel"]
+            return 0
+
+        # if state["score"] > 40:
+        #     self.epsilon = 0
+#self.gravity,
+        new_state = (self.gravity,
+					 state["monkey"]["vel"] // 10,
+                     # state["monkey"]["bot"] // 100,
+                     state["tree"]["dist"] // 120,
+                     np.mean([state["tree"]["top"] - state["monkey"]["top"], state["tree"]["bot"] - state["monkey"]["bot"]]) // 60)
 
         def q(state):
             if state not in self.qtable:
@@ -52,7 +67,10 @@ class Learner(object):
             self.last_action = np.argmax(q(self.last_state)) if npr.binomial(1, self.epsilon) == 0 else npr.binomial(1, 0.5)
             return self.last_action
 
-        self.qtable[self.last_state][self.last_action] = (1 - self.eta)*q(self.last_state)[self.last_action] + self.eta * (self.last_reward + self.gamma*np.max(q(new_state)))
+
+        # self.qtable[self.last_state][self.last_action] = (1 - self.eta)*q(self.last_state)[self.last_action] + self.eta * (self.last_reward + self.gamma*np.max(q(new_state)))
+        # SARSA next line
+        self.qtable[self.last_state][self.last_action] = (1 - self.eta)*q(self.last_state)[self.last_action] + self.eta * (self.last_reward + self.gamma*q(new_state)[np.argmax(q(new_state)) if npr.binomial(1, self.epsilon) == 0 else npr.binomial(1, 0.5)])
         self.last_state = new_state
         self.last_action = np.argmax(q(self.last_state)) if npr.binomial(1, self.epsilon) == 0 else npr.binomial(1, 0.5)
 
@@ -61,6 +79,8 @@ class Learner(object):
     def reward_callback(self, reward):
         '''This gets called so you can see what reward you get.'''
         self.last_reward = reward
+
+
 
 
 def run_games(learner, hist, iters = 100, t_len = 100):
@@ -84,6 +104,7 @@ def run_games(learner, hist, iters = 100, t_len = 100):
 
         # Reset the state of the learner.
         learner.reset()
+
     pg.quit()
     return
 
@@ -96,12 +117,13 @@ if __name__ == '__main__':
 	# Empty list to save history.
 	hist = []
 
+	n = 2000
 	# Run games.
-	run_games(agent, hist, 300, 1)
+	run_games(agent, hist, n, 1)
 
 	# Save history.
 	np.save('hist',np.array(hist))
 
 	print('max: ' + str(max(hist)))
 
-	print('average: ' + str(np.mean(np.array(hist))))
+	print('average: ' + str(np.mean(np.array(hist)[3*n//4:n])))
